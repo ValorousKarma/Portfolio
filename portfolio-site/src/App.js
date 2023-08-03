1335c4e59102f88b0cabb5ab47726e708b7a6dc7
ValorousKarma <dylanmichalak2003@gmail.com>
2023-08-03 00:49:38 -0700

    feat: add nav bar & basic styling

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,10 +1,35 @@
-import "./App.css";
+import "./styles/App.css";
+import React from 'react';
+import MaterialIcon, {colorPalette} from 'material-icons-react';
 
 function App() {
   return (
-    <header>
-      <h1>My Website</h1>
-    </header>
+    <div class="wrapper">
+      <Navbar />
+    </div>
+  );
+}
+
+const Navbar = () => {
+  return (
+    <nav class="main-nav">
+      <button>
+        <MaterialIcon icon="code" />
+        <span>Projects</span>
+      </button>
+      <button>
+        <MaterialIcon icon="school" />
+        <span>Education</span>
+      </button>
+      <button>
+        <MaterialIcon icon="work" />
+        <span>Experience</span>
+      </button>
+      <button>
+        <MaterialIcon icon="email" />
+        <span>Contact</span>
+      </button>
+    </nav>
   );
 }
 
